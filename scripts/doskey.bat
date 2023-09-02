90811bfb96954af04740bbd6677f0dd88a36a8ea
Zerio <neoboij@gmail.com>
2023-09-02 16:58:49 +0200

    Setup `doskey.bat`

---
+++ scripts/doskey.bat
@@ -0,0 +1,28 @@
+@echo off
+
+:: Linux commands
+doskey alias   = doskey $*
+doskey cat     = type $*
+doskey clear   = cls
+doskey cp      = copy $*
+doskey cpr     = xcopy $*
+doskey grep    = find $*
+doskey history = doskey /history
+doskey kill    = taskkill /PID $*
+doskey ls      = dir $*
+doskey man     = help $*
+doskey mv      = move $*
+doskey ps      = tasklist $*
+doskey pwd     = cd
+doskey rm      = del $*
+doskey rmr     = deltree $*
+doskey sudo    = runas /user:administrator $*
+doskey touch   = copy NUL $*
+
+:: Easier navigation
+doskey o       = start $*
+doskey oo      = start .
+doskey ..      = cd ..\$*
+doskey ...     = cd ..\..\$*
+doskey ....    = cd ..\..\..\$*
+doskey .....   = cd ..\..\..\..\$*
